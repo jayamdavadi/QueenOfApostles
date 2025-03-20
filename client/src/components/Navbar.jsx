c5fbc3d740b3779e17f9d6827d23a497cb34e29e
Abhishek-kam <mrfantastff@gmail.com>
2025-03-20 17:19:51 -0400

    Navbar Updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,11 +15,11 @@ export const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-lg">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <Link to="/" className="flex-shrink-0 flex items-center">
-              <span className="text-xl font-bold text-gray-800">Queen Of Apostels</span>
+              <span className="text-xl font-bold text-gray-800">Queen Of Apostles</span>
             </Link>
           </div>
 
